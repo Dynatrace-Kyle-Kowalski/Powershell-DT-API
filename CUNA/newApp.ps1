fc6925636c96db4885f6913a17e8b71a83d41480
Kowalski <kyle.kowalski@dynatrace.com>
2020-05-01 13:59:27 -0700

    ID identification and Configs by ID

--- CUNA/newApp.ps1
+++ CUNA/newApp.ps1
@@ -8,22 +8,57 @@ $token = 'XvhWZ00LRU27DkUmsyVBq'
 #"Dynamic" Parameters
 $apiversion = 'v1'
 
-
 #Create Header Object for request to use certian objects may add to this header
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 #Add Auth Header for API to use
 $headers.Add("Authorization", "Api-Token "+ $token)
 
 
-#build Auto Tags request
-$temp | requestBuilder -endpoint '/autoTags' 
-#Execute request against API
-$response | executeRequest -request $temp -method 'GET' -headers $headers 
 
-$response | ConvertTo-Json
+
+#build Auto Tags request
+$uri = '/autoTags/'
+$builtRequest = requestBuilder -endpoint $uri
+#Execute request against API
+$response = executeRequest -request $builtRequest -method 'GET' -headers $headers 
+
+#Build AutoTag/ID request for configurations
+$entityID = getIdValue -apiResponse $response -name "service"
+$uri = '/autoTags/'+ $entityID
+$builtRequest = requestBuilder -endpoint $uri
+#Execute request against API
+$response = executeRequest -request $builtRequest -method 'GET' -headers $headers 
+
+$response | ConvertTo-Json -Depth 24
+
+
+<# $newRules = addNewRule -entity "PROCESS_GROUP" -condtionKey "HOST_GROUP_NAME"  -conditionValue "FunctionTest"
+$newRules = $response.rules += $newRules
+
+ConvertTo-Json -InputObject $newRules -Depth 24
+
+ #>function addNewRule ($entity, $condtionKey, $conditionValue)
+{#Format rules string to update an exisiting rule configuration
+    $rulesJson = '{
+        "type":  "' + $entity + '",
+        "enabled":  true,
+        "valueFormat":  null,
+        "propagationTypes":  [
+                             ],
+        "conditions":  [
+                           {
+                               "key":  "{attribute=' + $condtionKey + '}",
+                               "comparisonInfo":  "{type=STRING; operator=CONTAINS; value=' + $conditionValue + '; negate=False; caseSensitive=False}"
+                           }
+                       ]
+    }'
+    ConvertFrom-Json -InputObject $rulesJson
+}
+
 
 function executeRequest ( $request , $method, $headers, $body )
 { #Execute api requests
+    #Write-Host $request
     Invoke-RestMethod $request -Method $method -Headers $headers -Body $body   
 } 
 
@@ -36,4 +71,11 @@ function requestBuilder($endpoint, $parameters)
         'https://' + $environment + '.' + $domain + '/api/config/' + $apiversion + $endpoint + "?" + $parameters
     }
     
+}
+
+function getIdValue($apiResponse ,$name)
+{#Query the ID list to find the id needed
+    #Write-Host $apiResponse
+    $return = $apiResponse.values | Where-Object {$_.name -eq $name}
+    $return.id
 }
