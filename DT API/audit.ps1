3142056d829405e01471d3fadbe8ddfea5106a9a
Kowalski <kyle.kowalski@dynatrace.com>
2020-06-10 14:07:15 -0700

    Config Audits done

--- DT API/audit.ps1
+++ DT API/audit.ps1
@@ -24,10 +24,16 @@ if($audit.environment -ieq "Prod"){
 
 $sEnv = $environments.nonProd
 
+function convertEpoch ($timestamp){#Convert Timestamp to Date-Time object
+    $origin = New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0
+    $origin.AddMilliseconds($timestamp)
+}
 
+
+#script start
 
 #get audit log from DT environment
-$log = getFromDTEnv -dtEnv $sEnv -endpoint '/auditlogs' -parameters 'pageSize=5000&from=-30d&to=now&sort=-timestamp' -api 'enviornment'
+$log = getFromDTEnv -dtEnv $sEnv -endpoint '/auditlogs' -parameters 'pageSize=5000&from=-30d&to=now&sort=timestamp' -api 'enviornment'
 #create running list of Audit Log to parse
 $auditList = $log.auditlogs
 #set variable to be used in next parameter call
@@ -40,9 +46,45 @@ while ($log.nextPageKey)
     $auditList += $log.auditlogs
     $nextPageKey = $log.nextPageKey
 }
+$accessLog = @()
+$accessLog += '"UserName","Timestamp"'
+
+$configLog = @()
+$configLog += '"UserName","Timestamp","Event Type","Entity","Log ID"'
 
 
-#TODO - Do something
+for ($i=0;$i -lt $auditList.Length; $i++){
+    if($auditList[$i].userOrigin -ieq "system"){
+        break;
+    }
+    if($auditList[$i].eventType -ieq "LOGIN"){
+        #build csv line
+        $accessLog += $auditList[$i].user + "," + (Get-Date -Date (convertEpoch -timestamp $auditList[$i].timestamp) -Format "MM/dd/yyyy HH:mm")
+        $al++
+    }else{
+        #build csv line
+        $date = convertEpoch -timestamp $auditList[$i].timestamp
+        $configLog += $auditList[$i].user + "," + (Get-Date -Date ($date) -Format "MM/dd/yyyy HH:mm") +','+ $auditList[$i].eventType + ',' + $auditList[$i].entityId + ',' + $auditList[$i].logId
+        $cl++
+    }
+}
 
-Write-Host $auditList
+$folder = Get-Date -Format "yyyyMMdd-HHmm"
+if (-not (Test-Path -Path "$path\Output\$folder")){#check if backups directory exisits if not create
+    New-Item -Path "$path\Output\" -Name $folder -ItemType "Directory"
+}
 
+#create and writeAccessLog
+New-Item -Path "$path\Output\$folder" -Name "AccessLog.csv" -ItemType "File" 
+for ($i=0;$i -lt $accessLog.Length; $i++){
+     Add-Content -Path "$path\Output\$folder\AccessLog.csv" -Value $accessLog[$i] 
+}
+
+New-Item -Path "$path\Output\$folder" -Name "ConfigLog.csv" -ItemType "File" 
+for ($i=0;$i -lt $configLog.Length; $i++){
+     Add-Content -Path "$path\Output\$folder\ConfigLog.csv" -Value $configLog[$i] 
+}
+
+
+$auditList = ConvertTo-Json -Depth 24 -InputObject $auditList
+New-Item -Path "$path\Output\$folder" -Name "AuditLogOutput.json" -ItemType "File" -Value $auditList
